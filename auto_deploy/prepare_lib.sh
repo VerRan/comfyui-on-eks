4bfef4aef83d51ce0754b03d626827eaf9fedc4f
VerRan <liu_ht@126.com>
2025-04-16 13:11:44 +0800

    add linxu env prepare shell

--- auto_deploy/prepare_lib.sh
+++ auto_deploy/prepare_lib.sh
@@ -1,29 +1,20 @@
-
 prepare_code_dependency() {
-    echo "==== Preparing code dependencies ===="
-    if [[ ! -d "$CDK_DIR" ]]; then
-        echo "CDK directory $CDK_DIR does not exist."
+    echo "==== Start preparing code ===="
+    cd $CDK_DIR && npm install --force && npm list && cdk bootstrap && cdk list
+    if [[ $? -ne 0 ]]
+    then
+        echo "Code preparation failed."
         exit 1
     fi
-    
-    cd "$CDK_DIR" || exit 1
-    npm install --force
-    npm list
-    
-    # Bootstrap CDK and list stacks
-    cdk bootstrap
-    cdk list
-    
-    # Update PROJECT_NAME if provided
-    if [[ -n "$PROJECT_NAME" ]]; then
-        if [[ "$OS_TYPE" == "macos" ]]; then
-            sed -i '' "s/export const PROJECT_NAME =.*/export const PROJECT_NAME = '${PROJECT_NAME}'/g" "$CDK_DIR/env.ts"
-        else
-            sed -i "s/export const PROJECT_NAME =.*/export const PROJECT_NAME = '${PROJECT_NAME}'/g" "$CDK_DIR/env.ts"
-        fi
-        echo "Stacks after updating PROJECT_NAME: $PROJECT_NAME"
-        cdk list
+    if [[ -z $PROJECT_NAME ]]
+    then
+        echo "PROJECT_NAME is not provided, use default empty."
     else
-        echo "PROJECT_NAME is not provided, using default empty value."
+        sed -i "s/export const PROJECT_NAME =.*/export const PROJECT_NAME = '${PROJECT_NAME}'/g" $CDK_DIR/env.ts
+        echo "Stacks after updating PROJECT_NAME: $PROJECT_NAME"
+        cd $CDK_DIR && cdk list
     fi
+    echo "==== Finish preparing code ===="
 }
+
+prepare_code_dependency
